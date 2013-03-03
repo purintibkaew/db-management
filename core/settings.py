9e0abe21e7315e215e6afa3b36b6b5727dff5570
Sergiy <semargal@gmail.com>
2013-03-03 16:40:46 +0200

    Added middleware

--- core/settings.py
+++ core/settings.py
@@ -105,6 +105,8 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.students.middleware.RequestStat',
+    # 'apps.students.middleware.AddStat'
 )
 
 ROOT_URLCONF = 'core.urls'
