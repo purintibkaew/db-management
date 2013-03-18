9fe44fff8a9b1a03b6454a3e9af19824fb81663a
Sergiy <semargal@gmail.com>
2013-03-18 17:26:19 +0200

    Added test

--- apps/students/tests.py
+++ apps/students/tests.py
@@ -1,16 +1,38 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
+
+from apps.students.models import Student, Group
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class StudentTest(TestCase):
+
+    fixtures = ['initial_data.json']
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_login(self):
+        response = self.client.get('/groups/')
+        self.assertNotEqual(response.status_code, 200)
+        response = self.client.post('/accounts/login/',
+                                    {'username': 'admin',
+                                     'password': 'admin'})
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get('/groups/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_group(self):
+        self.group = Group(
+            user=User.objects.get(id=1),
+            name='Test group'
+        )
+        self.group.save()
+        group = Group.objects.get(name='Test group')
+        self.assertEqual(group.id, self.group.id)
+        return self.group
+
+    def test_add_student_to_group(self):
+        student = Student.objects.get(id=1)
+        student.group = self.test_add_group()
+        student.save()
